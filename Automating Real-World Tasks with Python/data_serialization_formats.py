d99ed1f7d128ffa103b9b3e621a6c26efa32af72
yzeng44 <47905122+yzeng44@users.noreply.github.com>
2022-05-17 13:43:21 +0800

    Create data_serialization_formats.py

---
+++ Automating Real-World Tasks with Python/data_serialization_formats.py
@@ -0,0 +1,47 @@
+import json
+with open('people.json', 'w') as people_json:
+    json.dump(people, people_json, indent=2)
+
+# The contents of the file will look something like this:  
+[
+  {
+    "name": "Sabrina Green",
+    "username": "sgreen",
+    "phone": {
+      "office": "802-867-5309",
+      "cell": "802-867-5310"
+    },
+    "department": "IT Infrastructure",
+    "role": "Systems Administrator"
+  },
+  {
+    "name": "Eli Jones",
+    "username": "ejones",
+    "phone": {
+      "office": "684-348-1127"
+    },
+    "department": "IT Infrastructure",
+    "role": "IT Specialist"
+  },
+]
+
+import yaml
+
+with open('people.yaml', 'w') as people_yaml:
+    yaml.safe_dump(people, people_yaml)
+
+# The code will generate a 'people.yaml' that looks like this:
+- department: IT Infrastructure
+  name: Sabrina Green
+  phone:
+    cell: 802-867-5310
+    office: 802-867-5309
+  role: Systems Administrator
+  username: sgreen
+- department: IT Infrastructure
+  name: Eli Jones
+  phone:
+    office: 684-348-1127
+  role: IT Specialist
+  username: ejones
+
